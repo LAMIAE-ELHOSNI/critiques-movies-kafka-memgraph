137db1118877a8710a1f1ce1c80156672774d1e7
Fidelis <yonlifidelis2@gmail.com>
2023-11-26 18:51:56 +0100

    Requetes Cypher

--- src/memgraph/memgraph.py
+++ src/memgraph/memgraph.py
@@ -1,36 +1,30 @@
-from confluent_kafka import Consumer, KafkaException
+from confluent_kafka import Consumer
 import json
 import logging
-from gqlalchemy import Memgraph, MemgraphKafkaStream
-
+from neo4j import GraphDatabase
 
 # Configurer les logs
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Établir la connexion à Memgraph
-uri = "http://localhost:7687"
-username = "Fidelis"
-password = "admin"
-memgraph = None
+# Établir la connexion à Neo4j
+uri = "bolt://localhost:7687"
+username = ""  # Votre nom d'utilisateur
+password = ""  # Votre mot de passe
+driver = GraphDatabase.driver(uri, auth=(username, password))
 
 try:
-    memgraph = Memgraph(uri=uri, username=username, password=password)
-    logger.info("Connexion à Memgraph établie !")
-
     def transformer_donnees(message):
         try:
             # Convertir le message JSON en dictionnaire Python
             data = json.loads(message)
 
-            # Adapter la structure des données au format attendu par Memgraph
+            # Adapter la structure des données au format attendu par Neo4j
             transformed_data = {
                 "userId": data.get("userId", ""),
-                "movie": {
-                    "movieId": data.get("movieId", ""),
-                    "title": data.get("title", ""),
-                    "genres": data.get("genres", []),
-                },
+                "movieId": data.get("movie", {}).get("movieId", ""),
+                "title": data.get("movie", {}).get("title", ""),
+                "genres": data.get("movie", {}).get("genres", []),
                 "rating": data.get("rating", ""),
                 "timestamp": data.get("timestamp", "")
             }
@@ -41,36 +35,35 @@ try:
             logger.error(f"Erreur lors de la transformation des données : {e}")
             return None
 
-    def inserer_donnees_memgraph(data):
+    def inserer_donnees_neo4j(data):
         try:
-            # Query GQL pour insérer les données dans Memgraph
-            query = """
-                WITH $data AS data
-                MERGE (user:User {userId: data.userId})
-                MERGE (movie:Movie {movieId: data.movie.movieId})
-                ON CREATE SET movie.title = data.movie.title
+            with driver.session() as session:
+                # Query Cypher pour insérer les données dans Neo4j
+                query = """
+                    MERGE (user:User {userId: $userId})
+                    MERGE (movie:Movie {movieId: $movieId})
+                    ON CREATE SET movie.title = $title
 
-                FOREACH (genre IN data.movie.genres |
-                    MERGE (g:Genre {name: genre})
-                    MERGE (movie)-[:IS_GENRE]->(g)
-                )
+                    FOREACH (genre IN $genres |
+                        MERGE (g:Genre {name: genre})
+                        MERGE (movie)-[:IS_GENRE]->(g)
+                    )
 
-                MERGE (user)-[r:RATED]->(movie)
-                SET r.rating = toInteger(data.rating), r.timestamp = toInteger(data.timestamp)
-            """
+                    MERGE (user)-[r:RATED]->(movie)
+                    SET r.rating = toInteger($rating), r.timestamp = toInteger($timestamp)
+                """
 
-            # Exécuter la requête avec les données transformées
-            memgraph.execute(query, {"data": data})
-            logger.info(f"{data} inséré avec succès dans Memgraph !")
+                session.run(query, **data)
+                logger.info(f"{data} inséré avec succès dans Neo4j !")
 
         except Exception as e:
-            logger.error(f"Une erreur est survenue lors de l'insertion dans Memgraph : {e}")
+            logger.error(f"Une erreur est survenue lors de l'insertion dans Neo4j : {e}")
 
     def kafka_consumer():
         # Configuration du consommateur Kafka
         consumer_config = {
             'bootstrap.servers': 'localhost:9092',  # Adresse du broker Kafka
-            'group.id': 'my-group',  # ID du groupe de consommateurs
+            'group.id': 'my_group',  # ID du groupe de consommateurs
             'auto.offset.reset': 'earliest'  # Commencer à consommer depuis le début du topic
         }
 
@@ -93,11 +86,11 @@ try:
                     # Loguer les messages reçus depuis Kafka
                     logger.info(f"Message reçu depuis Kafka : {msg.value().decode('utf-8')}")
 
-                    # Transformer les données avant de les insérer dans Memgraph
+                    # Transformer les données avant de les insérer dans Neo4j
                     transformed_data = transformer_donnees(msg.value().decode('utf-8'))
 
                     if transformed_data:
-                        inserer_donnees_memgraph(transformed_data)
+                        inserer_donnees_neo4j(transformed_data)
                     else:
                         logger.error("Les données n'ont pas été transformées correctement.")
                 else:
@@ -105,8 +98,6 @@ try:
 
         except KeyboardInterrupt:
             logger.info("Arrêt du consommateur Kafka")
-        except KafkaException as kafka_exception:
-            logger.error(f"Erreur de connexion à Kafka : {kafka_exception}")
         except Exception as e:
             logger.error(f"Une erreur inattendue est survenue : {e}")
         finally:
@@ -114,20 +105,12 @@ try:
 
     # Appel du consommateur Kafka
     kafka_consumer()
-    
-
-    stream = MemgraphKafkaStream(name="ratings_stream", 
-                                 topics=["ratings"], 
-                                 transform="movielens.rating", 
-                                 bootstrap_servers="localhost:9093")
-    db.create_stream(stream)
-    db.start_stream(stream)
-    check = db.get_streams()
-
 
 except Exception as e:
-    logger.error(f"Une erreur inattendue est survenue lors de la connexion : {e}")
+    logger.error(f"Une erreur est survenue lors de la connexion à Neo4j : {e}")
+
 finally:
-    if memgraph is not None:
-        memgraph.close()
-        logger.info("Connexion à Memgraph fermée.")
+    # Fermeture de la connexion à Neo4j
+    if driver is not None:
+        driver.close()
+        logger.info("Connexion à Neo4j fermée.")
